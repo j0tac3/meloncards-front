4379c7699d8c4a1bcb2d939037007e563b2ebf17
Andrade . <j0s3.90jr@gmail.com>
2026-05-30 20:14:13 +0200

    first commit 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
-export const environment = {
+/* export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api' // La URL de tu servidor Laravel
+}; */
+
+export const environment = {
+  production: false,
+  apiUrl: 'hhttps://meloncards-backend.onrender.com/api' // La URL de tu servidor Laravel
 };
