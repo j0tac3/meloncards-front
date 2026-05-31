46a151a2ae436fb874f80dff8b74ad909553670e
Andrade . <j0s3.90jr@gmail.com>
2026-05-31 18:06:05 +0200

    limpieza de codigo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://api.melonapps.es' // La URL de tu servidor Laravel
+  apiUrl: 'https://api.melonapps.es/api' // La URL de tu servidor Laravel
 };
