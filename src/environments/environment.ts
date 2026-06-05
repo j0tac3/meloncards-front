aef343e820998528b8b3b6383d901bdfeaca02b9
Andrade . <j0s3.90jr@gmail.com>
2026-06-05 13:29:51 +0200

    Instala sharp oficialmente desde local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/api' // La URL de tu servidor Laravel
-};
-
 /* export const environment = {
   production: false,
-  apiUrl: 'https://api.melonapps.es/api' // La URL de tu servidor Laravel
+  apiUrl: 'http://localhost:8000/api' // La URL de tu servidor Laravel
 }; */
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.melonapps.es/api' // La URL de tu servidor Laravel
+};
